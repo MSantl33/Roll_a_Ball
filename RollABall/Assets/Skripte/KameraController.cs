bbef3c67f97572c2ce82474a94df4ea737b5ffd4
Matejas S <matejas.santl@student.mev.hr>
2020-06-06 13:38:12 +0200

    gotovo

---
+++ RollABall/Assets/Skripte/KameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 pomak;
+
+	// Use this for initialization
+	void Start () {
+		pomak = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + pomak;
+	}
+}
---
+++ RollABall/Assets/Skripte/Kretanje.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Kretanje : MonoBehaviour {
+
+	public Text countText;
+	public float speed;
+	private int zbroj;
+	private Rigidbody rb;
+
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCounterText ();
+	}
+		
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float kretanjeHorizontal = Input.GetAxis ("Horizontal");
+		float kretanjeVertical = Input.GetAxis ("Vertical");
+
+		Vector3 kretanje=new Vector3 (kretanjeHorizontal , 0.0f ,kretanjeVertical);
+
+		rb.AddForce(kretanje * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+
+			count += 1;
+			SetCounterText ();
+		}
+	}
+
+	void SetCounterText ()
+	{
+		countText.text = "Rezultat: " + count.ToString ();
+	}
+}
